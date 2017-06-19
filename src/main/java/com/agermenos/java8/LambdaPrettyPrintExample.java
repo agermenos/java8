2adb6aa02eae7b7fd22b4a7e3457af4b061fd21d
Alejandro Germenos\Home PC <agermenos@gmail.com>
2017-06-19 12:56:20 -0700

    First try with Lambdas (non existent yet)

--- src/main/java/com/agermenos/java8/LambdaPrettyPrintExample.java
+++ src/main/java/com/agermenos/java8/LambdaPrettyPrintExample.java
@@ -1,8 +1,40 @@
 package com.agermenos.java8;
 
+import java.util.Arrays;
+
 /**
  * Created by alejandrogermenos on 6/19/17.
  */
 public class LambdaPrettyPrintExample {
+    String brands[]=new String[] {"Gala", "Jazz", "Honey", "Sweet"};
+    String colors[]= new String[] {"red", "yellow", "dark red", "brown", "green"};
 
+    private Apple createRandomApple(){
+        int colorChosen = (int) (Math.random()*colors.length);
+        int brandChosen = (int) (Math.random()*brands.length);
+        int chosenWeight = 100 + (int) (Math.random()*200);
+        String color = colors[colorChosen];
+        String brand = brands [brandChosen];
+        return new Apple(brand, chosenWeight, color);
+    }
+
+    public Apple[] generateApples(int amount){
+        Apple[] returnSet = new Apple[amount];
+        for (int k=0; k<amount; k++) {
+            returnSet[k] = createRandomApple();
+        }
+        return returnSet;
+    }
+
+    public static void main(String args[]) {
+        PrettyPrinPredicate ppp = new PrettyPrinPredicate() {
+            public void print(Apple apple) {
+                System.out.printf("The apple %s has a weight of %doz and is of %s color\n", apple.getBrand(), apple.getWeight(), apple.getColor());
+            }
+        };
+
+        LambdaPrettyPrintExample lppe = new LambdaPrettyPrintExample();
+        Apple[] apples = lppe.generateApples(10);
+        Arrays.stream(apples).forEach(apple -> ppp.print(apple));
+    }
 }
--- src/main/java/com/agermenos/java8/PrettyPrinPredicate.java
+++ src/main/java/com/agermenos/java8/PrettyPrinPredicate.java
@@ -3,6 +3,6 @@ package com.agermenos.java8;
 /**
  * Created by alejandrogermenos on 6/19/17.
  */
-public class PrettyPrint {
-
+public interface PrettyPrinPredicate {
+    void print(Apple apple);
 }
